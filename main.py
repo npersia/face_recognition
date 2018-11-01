dc77bea4b45817d15a8d663fd8246fd3828d5ee7
Nahuel Persia <persianahuel@gmail.com>
2018-11-01 10:00:18 -0300

    se termino el trabajo de limpieza del codigo

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PIL import Image
 import os
 
 
-class Face():
+class Face:
 
     contador = 0
 
@@ -25,45 +25,31 @@ class Face():
         pil_image.save(self.file, "JPEG")
 
 
-
-class Faces():
+class Faces:
     def __init__(self):
         self.arr_faces = []
+        self.frame = None
+        self.arr_face_locations = []
+        self.arr_encode_faces = []
 
-    def addFace(self, face):
+    def add_face(self, face):
         self.arr_faces.append(face)
 
-    def getFaceName(self, name):
-        for f in self.arr_faces:
-            if f.name == name:
-                return f
-        return None
-
-    def existFaceName(self,name):
-        if self.getFaceName(name) != None:
-            return True
-        else:
-            return False
-
-    def locateFaces(self, frame):
+    def locate_faces(self, frame):
         self.frame = frame
-        self.arr_face_locations = face_recognition.face_locations(frame)
+        self.arr_face_locations = face_recognition.face_locations(self.frame)
 
-
-    def encodeFaces(self):
+    def encode_faces(self):
         self.arr_encode_faces = face_recognition.face_encodings(self.frame, self.arr_face_locations)
 
-
-    def saveFaces(self):
+    def save_faces(self):
 
         for f in self.arr_faces:
             f.on_screen = False
 
         i = 0
         for face_encoding in self.arr_encode_faces:
-            matches = face_recognition.compare_faces(self.getFacesEncoding(), face_encoding)
-
-            #indices = [i for i, x in enumerate(my_list) if x == "whatever"]
+            matches = face_recognition.compare_faces(self.get_faces_encoding(), face_encoding)
 
             indices = [i for i, x in enumerate(matches) if x == True]
 
@@ -74,9 +60,7 @@ class Faces():
                 for face_location in self.arr_face_locations:
                     a_face = Face(face_encoding, face_location)
                     a_face.save_face(self.frame)
-                    self.addFace(a_face)
-
-
+                    self.add_face(a_face)
             else:
                 first_match_index = matches.index(True)
                 loc_match_index = i
@@ -86,9 +70,7 @@ class Faces():
                 self.arr_faces[first_match_index].face_location = a
             i += 1
 
-
-
-    def getFacesEncoding(self):
+    def get_faces_encoding(self):
         """estos son los faces conocidos
         TODO cambiar nombre"""
         enc = []
@@ -96,18 +78,7 @@ class Faces():
             enc.append(f.encoded)
         return enc
 
-
-
-    def getFacesNames(self):
-        """estos son los faces conocidos por nombre
-        TODO cambiar nombre"""
-        nom = []
-        for f in self.arr_faces:
-            nom.append(f.name)
-        return nom
-
-
-    def displayNames(self,frame):
+    def display_names(self, frame):
         for face in self.arr_faces:
             if face.on_screen:
                 top, right, bottom, left = face.face_location
@@ -124,10 +95,7 @@ class Faces():
                 font = cv2.FONT_HERSHEY_DUPLEX
                 cv2.putText(frame, face.name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
-    def compareFaces(self,face_encoding):
-        return face_recognition.compare_faces(self.getFacesEncoding(), face_encoding)
-
-    def loadFaces(self, path):
+    def load_faces(self, path):
         images = os.listdir(path)
 
         for image in images:
@@ -135,45 +103,53 @@ class Faces():
             b = face_recognition.load_image_file(path + "/" + image)
             face_encoding = face_recognition.face_encodings(b)[0]
             a_face = Face(face_encoding, None)
-            self.addFace(a_face)
+            self.add_face(a_face)
 
 
-# Get a reference to webcam #0 (the default one)
-video_capture = cv2.VideoCapture(0)
+def process_video():
+    faces = Faces()
+    faces.load_faces("faces")
+
+    # Get a reference to webcam #0 (the default one)
+    video_capture = cv2.VideoCapture(0)
+
+    # esto es lo que uso para evitar que encodee en cada vuelta del while
+    while_counter = 0
+
+    while True:
+        while_counter += 1
+        # Grab a single frame of video
+        ret, frame = video_capture.read()
+
+        # Resize frame of video to 1/4 size for faster face recognition processing
+        small_frame = cv2.resize(frame, (0, 0), fx=1, fy=1)
+
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_small_frame = small_frame[:, :, ::-1]
+        # Only process every other frame of video to save time
+        if while_counter > 15:
+            while_counter = 0
+
+            faces.locate_faces(rgb_small_frame)
+            faces.encode_faces()
+            faces.save_faces()
+
+        faces.display_names(frame)
+        cv2.imshow('Video', frame)
+
+        # Hit 'q' on the keyboard to quit!
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 
-# esto es lo que uso para evitar que encodee en cada vuelta del while
-while_counter = 0
+    # Release handle to the webcam
+    video_capture.release()
+    cv2.destroyAllWindows()
 
 
-faces = Faces()
-faces.loadFaces("faces")
 
-while True:
-    while_counter += 1
-    # Grab a single frame of video
-    ret, frame = video_capture.read()
 
-    # Resize frame of video to 1/4 size for faster face recognition processing
-    small_frame = cv2.resize(frame, (0, 0), fx=1, fy=1)
 
-    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-    rgb_small_frame = small_frame[:, :, ::-1]
-    # Only process every other frame of video to save time
-    if while_counter > 15:
-        while_counter = 0
 
-        faces.locateFaces(rgb_small_frame)
-        faces.encodeFaces()
-        faces.saveFaces()
 
-    faces.displayNames(frame)
-    cv2.imshow('Video', frame)
-
-    # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# Release handle to the webcam
-video_capture.release()
-cv2.destroyAllWindows()
+process_video()
