dff8abad0df273ddcaeff75dafddf6d6976e0823
Nahuel Persia <persianahuel@gmail.com>
2018-11-01 09:03:59 -0300

    Inicio la tercer iteracion, donde se va a limpiar el codigo y simplificar al maximo la implementacion

--- main.py
+++ main.py
@@ -112,10 +112,10 @@ class Faces():
         for face in self.arr_faces:
             if face.on_screen:
                 top, right, bottom, left = face.face_location
-                top *= 4
-                right *= 4
-                bottom *= 4
-                left *= 4
+                top *= 1
+                right *= 1
+                bottom *= 1
+                left *= 1
 
                 # Draw a box around the face
                 cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
@@ -219,7 +219,7 @@ while True:
     ret, frame = video_capture.read()
 
     # Resize frame of video to 1/4 size for faster face recognition processing
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    small_frame = cv2.resize(frame, (0, 0), fx=1, fy=1)
 
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
     rgb_small_frame = small_frame[:, :, ::-1]
